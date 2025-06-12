07fe0eb8ead6f6ba0a3820f1bb21b6939e3b0c2e
Evgeniy Dmitriev <dmitriew85@yandex.ru>
2025-06-12 16:05:13 +0300

    update item, add booking

--- src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
